f618479a1ab5c31d6a408280e5535f62e520434d
dubeyswapnilshailesh-ai <dubeyswapnilshailesh@gmail.com>
2026-02-27 12:58:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function orderNow() {
+  alert("Thank you for choosing Sweet Scoops! 🍨 Order feature coming soon!");
+}
+
+function buyNow(flavor) {
+  alert("You selected " + flavor + "! 🛍️ Thank you for your interest!");
+}
